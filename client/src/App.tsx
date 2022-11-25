978482061d7a22ddd7fb0695ff6873aedb9e04be
Pasan Ratnayake <pasanratnayake@gmail.com>
2022-11-25 14:03:46 +0530

    Add Navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import './App.css';
+import HomePage from './containers/HomePage';
+
+const Appcontainer = styled.div`
+  ${tw`
+w-full 
+h-full 
+flex
+flex-col
+`}
+`;
 
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Appcontainer>
+      <HomePage />
+    </Appcontainer>
   );
 }
 
---
+++ client/src/containers/HomePage/index.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import Navbar from '../../components/Navbar';
+
+const PageContainer = styled.div`
+  ${tw`
+flex
+flex-col
+w-full
+h-full
+items-center 
+overflow-x-hidden
+`}
+`;
+
+const HomePage = () => {
+  return (
+    <PageContainer>
+      <Navbar/>
+    </PageContainer>
+  );
+};
+
+export default HomePage;
