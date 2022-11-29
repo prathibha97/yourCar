20360d46ea7d6bd6b6f600dba888a6865ecbc333
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-29 10:22:54 +0530

    Setup graphql api

--- server/src/cars/cars.resolver.ts
+++ server/src/cars/cars.resolver.ts
@@ -1,12 +1,25 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { CarsService } from './cars.service';
+import { NewCarInput } from './dtl/new-car-input';
+import { Car } from './entities/car';
 
 @Resolver()
 export class CarsResolver {
   constructor(private carsService: CarsService) {}
 
-  @Query(() => String)
-  public async cars() {
-    return 'Welcome to yourcar';
+  @Query(() => [Car])
+  public async cars(): Promise<Car[]> {
+    return await this.carsService.getAllCars().catch((err) => {
+      throw err;
+    });
+  }
+
+  @Mutation(() => Car)
+  public async addNewCar(
+    @Args('newCarData') newCarData: NewCarInput,
+  ): Promise<Car> {
+    return await this.carsService.addCar(newCarData).catch((err) => {
+      throw err;
+    });
   }
 }
--- server/src/cars/cars.service.ts
+++ server/src/cars/cars.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { NewCarInput } from './dtl/new-car-input';
+import { Car } from './entities/car';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  constructor(@InjectRepository(Car) private carRepository: Repository<Car>) {}
+  public async getAllCars(): Promise<Car[]> {
+    return await this.carRepository.find({}).catch((err) => {
+      throw new InternalServerErrorException();
+    });
+  }
+  public async addCar(newCarData: NewCarInput): Promise<Car> {
+    const newCar = this.carRepository.create(newCarData);
+    await this.carRepository.save(newCar).catch((err) => {
+      throw new InternalServerErrorException();
+    });
+    return newCar;
+  }
+}
